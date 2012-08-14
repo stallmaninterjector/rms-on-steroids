7d045a9d11996f0497011c46804a63807c756986
Richard Stallmanu <rms@gnu.org>
2012-08-13 22:14:27 -0400

    Do not sleep on Flood Detect

--- stallman.pl
+++ stallman.pl
@@ -524,7 +524,7 @@ sub interject {
     if ( $mechanize->status == "403"){print "Banned by Freedom-hating mods ;_;\n"; exit}
     if ( grep /successful/i, $mechanize->content()){print "Freedom Delivered!\n\n"} 
     if ( grep /mistyped/i, $mechanize->content()){print "Mistyped Captcha\n"; &interject($url, $post_no, $page); return} 
-    if ( grep /flood/i, $mechanize->content()){print "Flood Detected\n"} 
+    if ( grep /flood/i, $mechanize->content()){print "Flood Detected\n"; print "\n"; return} 
     if ( grep /duplicate/i, $mechanize->content()){print "Duplicate Image\n"} 
 
     sleep($min_post_interval + rand($post_interval_variation)); 
@@ -541,7 +541,7 @@ sub select_pic {
 #   Select a file from the array, resize it, and give it a random unix timestamp.
     log "No RMS pictures in folder... ;_;\n" && exit if ! @handsome_rms_pics;
     my $filename = "/tmp/" . int(time() - rand(9999999)) . int(rand(888) + 100) . ".jpg";
-    if ( $rainbow_rms ){system 'convert "' . @handsome_rms_pics[int(rand(@handsome_rms_pics))] . '" -resize ' . int(rand(20)+ 80) . '% -modulate 100,100,' . int(rand(199)) . ' '  . $filename;}
+    if ( $rainbow_rms ){system 'convert "' . @handsome_rms_pics[int(rand(@handsome_rms_pics))] . '" -resize ' . int(rand(20)+ 80) . '% -modulate 100,100,' . int(rand(999)) . ' '  . $filename;}
     else {system 'convert "' . @handsome_rms_pics[int(rand(@handsome_rms_pics))] . '" -resize ' . int(rand(20)+ 80) . '% ' . $filename;}
     return $filename;
 }
